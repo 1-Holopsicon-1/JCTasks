b7eeab35d01406d944aa7f63e2a1e35f15a8a488
1-Holopsicon-1 <mfnbrx@gmail.com>
2024-11-20 13:08:30 +0300

    seventh Task

--- main.go
+++ main.go
@@ -2,54 +2,51 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
+	"time"
 )
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	ch3 := make(chan int)
-	ch4 := make(chan int)
-
-	go ChanRandomiser(5, 100, ch1)
-	go ChanRandomiser(5, 100, ch2)
-	go ChanRandomiser(5, 100, ch3)
-	go ChanRandomiser(5, 100, ch4)
-	merged := MergeChanels(ch1, ch2, ch3, ch4)
-	fmt.Print("Meged chan: ")
-	for v := range merged {
-		fmt.Printf("%v ", v)
-	}
-}
-
-func MergeChanels(chans ...<-chan int) <-chan int {
-	merge := make(chan int)
-
-	go func() {
-		var wg sync.WaitGroup
-		readFromCh := func(ch <-chan int) {
+	var wg = NewSemaphoreWait(3)
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
 			defer wg.Done()
-			for v := range ch {
-				merge <- v
-			}
-		}
-		for _, v := range chans {
-			wg.Add(1)
-			go readFromCh(v)
-		}
-		wg.Wait()
-		close(merge)
-	}()
+			fmt.Printf("Горутина %d начала работу\n", i)
+			time.Sleep(time.Second)
+			fmt.Printf("Горутина %d завершила работу\n", i)
+		}(i)
+	}
 
-	return merge
+	wg.Wait()
+	fmt.Println("Все горутины завершены.")
 }
 
-func ChanRandomiser(amount, maxValue int, randomCh chan<- int) {
+type SemaphoreWait struct {
+	semaphore chan struct{}
+	count     int
+}
 
-	for i := 0; i < amount; i++ {
-		num := rand.Intn(maxValue)
-		randomCh <- num
+func NewSemaphoreWait(n int) *SemaphoreWait {
+	return &SemaphoreWait{
+		semaphore: make(chan struct{}, n),
+		count:     0,
 	}
-	close(randomCh)
+}
+
+func (wg *SemaphoreWait) Add(delta int) {
+	wg.count += delta
+	for i := 0; i < delta; i++ {
+		wg.semaphore <- struct{}{}
+	}
+}
+
+func (wg *SemaphoreWait) Done() {
+	wg.count--
+	<-wg.semaphore
+}
+
+func (wg *SemaphoreWait) Wait() {
+	for wg.count > 0 {
+		time.Sleep(10 * time.Millisecond)
+	}
 }
--- task_test.go
+++ task_test.go
@@ -1,42 +1,32 @@
 package main
 
 import (
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestMergeChanels(t *testing.T) {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	ch3 := make(chan int)
-	go func() {
-		for i := 0; i < 5; i++ {
-			ch1 <- i
-		}
-		close(ch1)
-	}()
-	go func() {
-		for i := 5; i < 10; i++ {
-			ch2 <- i
-		}
-		close(ch2)
-	}()
-	go func() {
-		for i := 10; i < 15; i++ {
-			ch3 <- i
-		}
-		close(ch3)
-	}()
-	merged := MergeChanels(ch1, ch2, ch3)
-	expected := make(map[int]interface{})
-	for i := 0; i < 15; i++ {
-		expected[i] = ""
-	}
-	var findUnmatched bool
-	for v := range merged {
-		_, findUnmatched = expected[v]
-		if !findUnmatched {
-			t.Errorf("Error cant find existing variable")
-		}
-	}
+func TestSemaphoreWait(t *testing.T) {
+	const maxGoroutines = 3
+	const totalGoroutines = 10
 
+	var wg = NewSemaphoreWait(maxGoroutines)
+	var mu sync.Mutex
+	var counter int
+
+	for i := 0; i < totalGoroutines; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			time.Sleep(100 * time.Millisecond)
+			mu.Lock()
+			counter++
+			mu.Unlock()
+		}(i)
+	}
+	wg.Wait()
+
+	if counter != totalGoroutines {
+		t.Errorf("Ожидалось завершение %d горутин, завершено %d", totalGoroutines, counter)
+	}
 }
